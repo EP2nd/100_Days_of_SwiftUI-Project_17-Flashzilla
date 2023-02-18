0006a86aa8b0a9b2117fe37e7cfa27311420d71b
Edwin Przeźwiecki Jr <edwin.przezwiecki.jr@icloud.com>
2023-02-18 21:56:29 +0100

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Edwin Przeźwiecki Jr. on 18/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
