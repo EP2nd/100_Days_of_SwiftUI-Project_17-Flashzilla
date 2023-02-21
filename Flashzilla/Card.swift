94e9f30ad53907120a22d4f799e4f164080eccc5
Edwin Przeźwiecki Jr <edwin.przezwiecki.jr@icloud.com>
2023-02-21 02:52:02 +0100

    Further progression

---
+++ Flashzilla/Card.swift
@@ -0,0 +1,15 @@
+//
+//  Card.swift
+//  Flashzilla
+//
+//  Created by Edwin Przeźwiecki Jr. on 20/02/2023.
+//
+
+import Foundation
+
+struct Card {
+    let prompt: String
+    let answer: String
+    
+    static let example = Card(prompt: "Who played the 13th Doctor in Doctor Who?", answer: "Jodie Whittaker")
+}
---
+++ Flashzilla/CardView.swift
@@ -0,0 +1,79 @@
+//
+//  CardView.swift
+//  Flashzilla
+//
+//  Created by Edwin Przeźwiecki Jr. on 20/02/2023.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    
+    @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
+    
+    let card: Card
+    
+    var removal: (() -> Void)? = nil
+    
+    @State private var isShowingAnswer = false
+    @State private var offset = CGSize.zero
+    
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 25, style: .continuous)
+                .fill(
+                    differentiateWithoutColor
+                    ? .white
+                    : .white
+                        .opacity(1 - Double(abs(offset.width / 50)))
+                )
+                .background(
+                    differentiateWithoutColor
+                    ? nil
+                    : RoundedRectangle(cornerRadius: 25, style: .continuous)
+                        .fill(offset.width > 0 ? .green : .red)
+                )
+                .shadow(radius: 10)
+            
+            VStack {
+                Text(card.prompt)
+                    .font(.largeTitle)
+                    .foregroundColor(.black)
+                
+                if isShowingAnswer {
+                    Text(card.answer)
+                        .font(.title)
+                        .foregroundColor(.gray)
+                }
+            }
+            .padding(20)
+            .multilineTextAlignment(.center)
+        }
+        .frame(width: 450, height: 250)
+        .rotationEffect(.degrees(Double(offset.width / 5)))
+        .offset(x: offset.width * 5, y: 0)
+        .opacity(2 - Double(abs(offset.width / 50)))
+        .gesture(
+            DragGesture()
+                .onChanged { gesture in
+                    offset = gesture.translation
+                }
+                .onEnded { _ in
+                    if abs(offset.width) > 100 {
+                        removal?()
+                    } else {
+                        offset = .zero
+                    }
+                }
+        )
+        .onTapGesture {
+            isShowingAnswer.toggle()
+        }
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardView(card: Card.example)
+    }
+}
